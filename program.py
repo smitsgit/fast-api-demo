103ee6ac2e3f1df22f6d7fc7d6f87e144754a03e
Smital <smital@smital.desai>
2020-06-29 10:15:56 +0530

    Initial commit

---
+++ program.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, ValidationError
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
+
+
+@app.get("/")
+def hello_world():
+    return {'name': 'Smital', 'surname': 'Desai'}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str):
+    return {'item_id': item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    data = {'name': "Smital", 'price': "test", 'is_offer': True}
+    dm = Item(name=10, price='hello', is_offer=10)
+    return {"item_price": item.price}
